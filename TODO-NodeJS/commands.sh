8e05d48256c92532e0048d4322bc72ee3d89391b
Firas Chbiki <firas.chbiki@sofrecom.com>
2021-03-14 23:08:50 +0100

    fixed db docker compose

--- TODO-NodeJS/commands.sh
+++ TODO-NodeJS/commands.sh
@@ -1,6 +1,6 @@
 docker build -t todobackend .
 
 docker run --name backend -d \
--e DB_HOST="10.0.0.4" --network db-network \
+-e DB_HOST="mydb" --network db-network \
 -e DB_PORT="3306" -e DB_USER="user" -e DB_PASS="pass" -e DB_NAME="testdb" \
 -e NODE_ENV="dev" -e PORT="1338" -p 1338:1338 todobackend
